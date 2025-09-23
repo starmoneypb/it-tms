5bf0a55eed2660c2b0bbc09b936b06eca3129f21
13cosmicboys <13cosmicboys@gmail.com>
2025-09-24 03:28:14 +0700

    initial commit

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -3,6 +3,7 @@ import js from "@eslint/js";
 export default [
   js.configs.recommended,
   {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
